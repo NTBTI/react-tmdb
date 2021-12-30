122ddd2b54ff393f14cd760d014d7d259b7068af
Tom <tom@ntbti.com>
2021-12-29 23:10:22 -0500

    Added fetching movies from API Before custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 //why no folder? Becuase we make homepage a container, we style every piece inside it instead
 //thus there is no need for a style file here
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+//API
+import API from '../API';
 
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
@@ -25,8 +28,35 @@ const Home = () => {
     //but don't
     */
 
+    /* important! Becuase we have 3 states here, we will get 3 re-renders if we touch any ONE of them */
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    //in the API code it uses await so we must mark this async
+    const fetchMovies = async(page, seaerchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(seaerchTerm, page);
+            console.log(movies);
+
+            //if you give this a function, it will call it with the previous state
+            //also never never never set the state manually, always call a setState
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results] //this appends more movies if the "prev" has some already else set the movies
+            }))
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchMovies(1);
+    }, []); //this is a dependency array. empty means this will only fire once (when the page renders)
 
     return <div>Home Page</div>
 }
