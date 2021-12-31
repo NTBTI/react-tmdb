d680b59625c262a60cffa553f0302c46821f8d52
Tom <tom@ntbti.com>
2021-12-30 23:23:08 -0500

    Breadcrumbs component created MovieInfo component created Before bottom info bar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,16 +7,29 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import Breadcrumb from './Breadcrumb';
+import MovieInfo from './MovieInfo';
 
 //Hooks
 import { useMovieFetch } from '../hooks/useMovieFetch';
 
+
 const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loadingState: loading, error } = useMovieFetch(movieId);
+
+    if (loading) {
+        return <Spinner/>
+    }
+
+    if (error) {
+        return <div>Something Went Wrong</div>
+    }
+
     return (
         <>
-            <div>Movie</div>
+            <Breadcrumb movieTitle={movie.original_title}/>
+            <MovieInfo movie={movie}/>
         </>
     )
 };
